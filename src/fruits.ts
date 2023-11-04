8d9418e91bf3c4de4347f50b250e559456658a4d
kimbeomgyu <bg.kim@ejn.gg>
2023-11-04 23:15:13 +0900

    경로 수정

--- src/fruits.ts
+++ src/fruits.ts
@@ -1,46 +1,46 @@
 export const FRUITS = [
   {
-    src: "/images/00_cherry.png",
+    src: "images/00_cherry.png",
     radius: 33 / 2,
   },
   {
-    src: "/images/01_strawberry.png",
+    src: "images/01_strawberry.png",
     radius: 48 / 2,
   },
   {
-    src: "/images/02_grape.png",
+    src: "images/02_grape.png",
     radius: 61 / 2,
   },
   {
-    src: "/images/03_gyool.png",
+    src: "images/03_gyool.png",
     radius: 69 / 2,
   },
   {
-    src: "/images/04_orange.png",
+    src: "images/04_orange.png",
     radius: 89 / 2,
   },
   {
-    src: "/images/05_apple.png",
+    src: "images/05_apple.png",
     radius: 114 / 2,
   },
   {
-    src: "/images/06_pear.png",
+    src: "images/06_pear.png",
     radius: 129 / 2,
   },
   {
-    src: "/images/07_peach.png",
+    src: "images/07_peach.png",
     radius: 156 / 2,
   },
   {
-    src: "/images/08_pineapple.png",
+    src: "images/08_pineapple.png",
     radius: 177 / 2,
   },
   {
-    src: "/images/09_melon.png",
+    src: "images/09_melon.png",
     radius: 220 / 2,
   },
   {
-    src: "/images/10_watermelon.png",
+    src: "images/10_watermelon.png",
     radius: 259 / 2,
   },
 ];
